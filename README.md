4a8b030372711906c1e24b27fdb0bd3144db5597
Ilan Bigio <ilanbigio@gmail.com>
2022-06-24 19:57:39 +0200

    First commit, ready to test.

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# shell-ai
+# ShellAI
 A fantastic little AI Shell Assistant for my friends, powered by GPT3.
+
+## Setup
+
+1. Install Deno. (https://deno.land/manual/getting_started/installation)
+2. Compile the executable. (This will create an executable called `shell_ai`.)
+```
+deno compile --allow-net --allow-env --allow-read shell_ai.ts
+```
+
+
+3. Save the executable in a bin. If you don't have one, follow steps 4 and 5.
+
+4. Create a new bin.
+```
+mkdir -p ~/CustomBin/bin
+mv shell_ai ~/CustomBin/bin
+```
+
+5. Add the bin to your path. You should also add the following line to your `.zshrc` or `.bashrc`:
+```
+export PATH=$PATH:~/CustomBin/bin
+```
+
+6. Rename the executable to a short name of your chosing. I like 'q'.
+```
+mv ~/CustomBin/bin/shell_ai ~/CustomBin/bin/q
+```
+
+7. Set the `SHELL_AI_KEY` environment variable to your user key. Add this line to your `.zashrc` or `.bashrc` as well!
+```
+export SHELL_AI_KEY="[insert key here]"
+```
+
+8. Open a new shell and have fun!
+
+## Usage
+
+Type `q` followed by a description of a shell command you want to write!
+
+Nice for beginners...
+```
+$ q make a new git branch
+git checkout -b "new-branch"
+```
+
+...and those who forget how to use `find`, like me.
+```
+$ q find files that contain "administrative" in the name
+find . -name "*administrative*"
+```
---
+++ shell_ai.ts
@@ -0,0 +1,68 @@
+const user_key = Deno.env.get("SHELL_AI_KEY");
+if (user_key == null) {
+  console.log("======================================================");
+  console.log("                        Hello!                        ");
+  console.log("======================================================");
+  console.log("");
+  console.log(
+    "You're seeing this greeting because the SHELL_AI_KEY\nenvironment variable is not set. Make sure to set it\nto your user key with the following command:"
+  );
+  console.log("");
+  console.log('  export SHELL_AI_KEY="[insert key here]"');
+  console.log("");
+  console.log(
+    "To avoid having to do this in future terminal sessions,\nyou can also add the above line to your .zshrc or\n.bashrc!"
+  );
+  console.log("");
+  console.log(
+    "I recommend putting this executable in a bin, and\nrenaming it to 'q' (or some other easy command). You\ncan do that like this:"
+  );
+  console.log("");
+  console.log("  mkdir -p ~/CustomBin/bin");
+  console.log("  mv " + Deno.execPath() + " ~/CustomBin/bin/g");
+  console.log("  export PATH=$PATH:~/CustomBin/bin/g");
+  console.log("");
+  console.log(
+    "You should add that last line to your .zshrc or\n.bashrc as well!"
+  );
+  console.log("");
+  console.log("(If you don't have a user key, ask Ilan!)");
+  Deno.exit(1);
+}
+
+if (Deno.args.length == 0) {
+  console.log("usage: g desired command text");
+  Deno.exit(1);
+}
+
+async function run_client() {
+  const phrase = Deno.args.join(" ");
+  const response = await fetch("https://sweet-spider-89.deno.dev", {
+    method: "POST",
+    body: JSON.stringify({
+      userID: user_key,
+      phrase: phrase,
+    }),
+  });
+
+  if (response.status != 200) {
+    throw response.statusText;
+  }
+
+  try {
+    const j = await response.json();
+    const completion = j?.completion;
+    console.log(completion);
+  } catch {
+    console.log("error: unable to parse response");
+    Deno.exit(1);
+  }
+}
+
+try {
+  await run_client();
+} catch (e) {
+  console.log(e);
+  console.log("error: command failed");
+  Deno.exit(1);
+}
